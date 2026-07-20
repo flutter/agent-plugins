ff95079887f0ccbc0abb2ab5cbd8a870ec01eac3
Reid Baker <reidbaker@google.com>
2026-07-20 16:22:25 -0400

    Ensure lint_skills_test is path-resilient and asserts non-empty directoryConfigs

--- tool/generator/test/lint_skills_test.dart
+++ tool/generator/test/lint_skills_test.dart
@@ -2,8 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:dart_skills_lint/dart_skills_lint.dart';
 import 'package:logging/logging.dart';
+import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 
 import 'custom_skill_rules/last_modified_rule.dart';
@@ -16,9 +19,22 @@ void main() {
     });
 
     try {
-      final config = await ConfigParser.loadConfig();
+      final String packageDir = Directory.current.path.endsWith('generator')
+          ? Directory.current.path
+          : p.join(Directory.current.path, 'tool', 'generator');
+      final String configPath = p.join(packageDir, 'dart_skills_lint.yaml');
+      final String skillsDir = p.normalize(p.join(packageDir, '..', '..', 'skills'));
+
+      final config = await ConfigParser.loadConfig(path: configPath);
+      expect(
+        config.directoryConfigs,
+        isNotEmpty,
+        reason: 'Configuration directoryConfigs should not be empty.',
+      );
+
       expect(
         await validateSkills(
+          skillDirPaths: [skillsDir],
           config: config,
           customRules: [LastModifiedRule()],
         ),
