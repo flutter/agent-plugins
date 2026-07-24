bc82b1f1b3f861817255e7764ac2fdb19011d7f3
Reid Baker <reidbaker@google.com>
2026-07-23 23:21:54 -0400

    Add copyright headers to test fixtures

--- tool/dart_skills_lint/evals/test_data/bad_code/tmp/bad_script.dart
+++ tool/dart_skills_lint/evals/test_data/bad_code/tmp/bad_script.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // ignore_for_file: unused_local_variable, prefer_final_locals, avoid_print, use_raw_strings
 
 // Violates placement hygiene (in tmp/ instead of bin/ or lib/)
